5eefcb7f8404c96d7c428d5645028e128b10f609
Wei Shi <shixwei@gmail.com>
2019-10-31 19:40:48 -0500

    Edited introduction text

--- client/src/Components/Introduction.js
+++ client/src/Components/Introduction.js
@@ -6,7 +6,7 @@ class Introduction extends Component {
     return (
       <div className="introduction-container">
         <h1 className = "introduction-header">Hi! Nice to meet you.</h1>
-        <p className="introduction-text">I'm an avid programmer looking for my next software engineering position</p>
+        <p className="introduction-text">I'm an avid programmer looking for full-time positions</p>
         <hr />
       </div>
     );
